9af4038e61e8c77c497c7f237da31399505e00a8
Vadim Korolik <korolik@usc.edu>
2017-01-11 13:58:29 -0800

    docker run script cleanup

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-docker run --rm -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix aruco
+docker run --rm -it \
+    -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    aruco /aruco/run_demo.sh
